b3b1c77b765707cf9f59d6ffdeddaa6e46ecbc00
Pcjmy <1716045191@qq.com>
2024-08-09 22:50:05 +0800

    feat: readFile

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
+const fs = require("fs"); 
+
+// Read users.json file 
+fs.readFile("package.json", function(err, data) { 
+    
+    // Check for errors 
+    if (err) throw err; 
+
+    // Converting to JSON 
+    const pkg = JSON.parse(data); 
+    console.log(pkg?.dependencies); // Print users 
+}); 
 
 console.log('welcome dep-check');
