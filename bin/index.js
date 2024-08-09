8fed8bdac048d2ed95c80b3c29547d2c36f9ef82
Pcjmy <1716045191@qq.com>
2024-08-09 23:01:08 +0800

    feat: react dependencies

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
-const fs = require("fs"); 
+const fs = require('fs');
+const ls = require('npm-remote-ls').ls;
 
-// Read users.json file 
-fs.readFile("package.json", function(err, data) { 
+// // Read users.json file 
+// fs.readFile("package.json", function(err, data) { 
     
-    // Check for errors 
-    if (err) throw err; 
+//     // Check for errors 
+//     if (err) throw err; 
 
-    // Converting to JSON 
-    const pkg = JSON.parse(data); 
-    console.log(pkg?.dependencies); // Print users 
-}); 
+//     // Converting to JSON 
+//     const pkg = JSON.parse(data); 
+//     console.log(pkg?.dependencies); // Print users 
+// }); 
 
 console.log('welcome dep-check');
+
+ls('react', '18.0.0', true, function(tree) {
+  console.log(tree);
+});
