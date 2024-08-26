c454bec0eb3f057cc08bbc9542c47ec160cd5006
Pcjmy <1716045191@qq.com>
2024-08-27 00:12:06 +0800

    fix: addEdge

--- src/utils/graph.js
+++ src/utils/graph.js
@@ -1,10 +1,9 @@
 import { getPackageDependencies, getLatestVersion, getVersion } from './dep';
 
 class Node {
-  constructor(name, version, depth) {
+  constructor(name, version) {
     this.name = name;
     this.version = version;
-    this.depth = depth;
   }
 }
 
@@ -26,33 +25,33 @@ class Graph {
       this.addVertex(node2);
     }
     this.adjacencyList.get(node1).push(node2);
-    this.adjacencyList.get(node2).push(node1);
   }
 
   bfs() {
     const colors = ['red', 'aqua', 'orange', 'yellow', 'pink', 'purple']
     const nodes = []
     const links = []
-    const queue = [this.start];
+    const queue = [{ node: this.start, depth: 1}];
     const start_node = {id: `${this.start.name}@${this.start.version}`, color: colors[0]};
     nodes.push(start_node)
     const visited = new Set();
     visited.add(start_node.id);
 
     while (queue.length) {
-      const node = queue.shift();
+      const item = queue.shift();
       // console.log(node);
-      const neighbors = this.adjacencyList.get(node);
+      const neighbors = this.adjacencyList.get(item.node);
       console.log('neighbors=', neighbors);
       if (neighbors) {
         for (const neighbor of neighbors) {
-          const v1 = {id: `${node.name}@${node.version}`, color: colors[node.depth - 1]}
-          const v2 = {id: `${neighbor.name}@${neighbor.version}`, color: colors[neighbor.depth - 1]}
+          const v1 = {id: `${item.node.name}@${item.node.version}`, color: colors[item.depth - 1]}
+          const v2 = {id: `${neighbor.name}@${neighbor.version}`, color: colors[item.depth]}
+          console.log(v1, v2);
           if (!visited.has(v2.id)) {
-            links.push({source: v1.id, target: v2.id})
             nodes.push(v2);
             visited.add(v2.id);
-            queue.push(neighbor);
+            links.push({source: v1.id, target: v2.id});
+            queue.push({ node: neighbor, depth: item.depth + 1 });
           }
         }
       }
@@ -67,25 +66,25 @@ export const getNpmDepGraph = async (packageName, version, maxDepth = 2) => {
   if (packageVersion === 'latest') {
     packageVersion = await getLatestVersion(packageName);
   }
-  const start = new Node(packageName, packageVersion, 1);
+  const start = new Node(packageName, packageVersion);
   const graph = new Graph(start);
 
-  const queue = [start];
+  const queue = [{ node: start, depth: 1 } ];
   const visited = new Set();
   visited.add(start);
 
   while (queue.length) {
-    const node = queue.shift();
-    if (node.depth > maxDepth) return graph;
-    const dependencies = await getPackageDependencies(node.name, getVersion(node.version));
+    const item = queue.shift();
+    if (item.depth > maxDepth) return graph;
+    const dependencies = await getPackageDependencies(item.node.name, getVersion(item.node.version));
     if (dependencies) {
       for (const [name, version] of Object.entries(dependencies)) {
-        const child = new Node(name, version, node.depth + 1);
-        graph.addEdge(node, child);
-        // console.log(node, child);
+        const child = new Node(name, version);
+        graph.addEdge(item.node, child);
+        // console.log(item.node, child);
         if (!visited.has(child)) {
           visited.add(child);
-          queue.push(child);
+          queue.push({ node: child, depth: item.depth + 1 });
         }
       }
     }
